7365134587ebfe8a7b4459a41be7ac9905916f5d
Jonatan Silva <jonatanda25@gmail.com>
2022-05-12 12:39:29 -0300

    alterando tela principal

--- lib/PagePrincipal.dart
+++ lib/PagePrincipal.dart
@@ -12,7 +12,9 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("alugar"),
+        title: Text(
+          "alugar",
+        ),
       ),
     );
   }
